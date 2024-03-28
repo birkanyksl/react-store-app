6addac15a1dc4cd023e5e3583132720a6d8ec79a
Birkan <birkanyksl613@gmail.com>
2024-03-29 01:02:32 +0300

    Cart componenti oluşturuldu.
    css özellikleri verildi.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,25 @@
+import "./Cart.css";
+
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <div className="offcanvas">
+      <div className="content">
+        <div className="cart-head">
+          <h2>Sepetim</h2>
+          <a href="/">X</a>
+        </div>
+        cartItems
+        <div className="total">
+          <span>Toplam Değer</span>
+          <span>10₺</span>
+        </div>
+        <div className="actions">
+          <button className="cart-order">Sipariş Ver</button>
+          <button className="cart-clear">Temizle</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
