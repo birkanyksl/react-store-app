dd88fd28dcbe7aa75af84c53bd5516f4a7daa405
Birkan <birkanyksl613@gmail.com>
2024-04-02 01:46:14 +0300

    context api kuruldu.
    Cart componenti için CartProvider oluşturuldu.

---
+++ src/context/CartProvider.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import { createContext } from "react";
+
+const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: () => {},
+    removeItem: () => {},
+    clearItem: () => {},
+  };
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
+};
+
+export default CartProvider;
